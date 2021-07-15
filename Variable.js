6e870ceb3011da42c52bf1b820185e2098e68344
MahmudulRafi <rafi.mahmudul.cse@gmail.com>
2021-07-15 09:57:01 +0600

    JavaScript Variables

---
+++ Variable.js
@@ -0,0 +1,17 @@
+// Variable
+
+var carPrice = 10000; 
+console.log(carPrice);
+console.log(typeof(carPrice));
+
+var myName = "Mahmudul Rafi";
+console.log(myName);
+console.log(typeof(myName));
+console.log(myName.length);
+console.log(myName.toLowerCase());
+console.log(myName.indexOf("Rafi"));
+console.log(myName.split(" "));
+
+var tepmeratureIsCool = false;
+console.log(tepmeratureIsCool);
+console.log(typeof(tepmeratureIsCool));
