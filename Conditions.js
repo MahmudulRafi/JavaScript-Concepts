a6ffd6c7bcf0bcd923641627ef5bad425ec201db
MahmudulRafi <rafi.mahmudul.cse@gmail.com>
2021-07-15 12:50:01 +0600

    Conditions in JS

---
+++ Conditions.js
@@ -0,0 +1,16 @@
+// Conditions in JavaScript
+
+var restaurantOpen = true;
+var foodPrice = 250;
+
+if (restaurantOpen == true) {
+    if (foodPrice < 200) {
+        console.log("Lets eat");
+    }
+    else {
+        console.log("Will ate later");
+    }
+}
+else {
+    console.log("Wait to open the restaurant.")
+}
