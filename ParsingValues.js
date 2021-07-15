ce9d05517931bccb89276bba7a625337a77e3394
MahmudulRafi <rafi.mahmudul.cse@gmail.com>
2021-07-15 11:07:29 +0600

    Parsing Values in JavaScript

---
+++ ParsingValues.js
@@ -0,0 +1,9 @@
+// Parsing Values
+
+var firstNumber = 10;
+var secondNumber = 25.35;
+var thirdNumber = '30.12';
+
+thirdNumber = parseFloat(thirdNumber);
+
+console.log(firstNumber + secondNumber + thirdNumber);
