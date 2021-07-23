41a02d0854fda3085c72b485da343779b72c2296
MahmudulRafi <rafi.mahmudul.cse@gmail.com>
2021-07-23 23:01:46 +0600

    Switch in JavaScript

---
+++ Switch.js
@@ -0,0 +1,21 @@
+// Switch Case
+
+number = 10;
+
+switch (number) {
+    case 1:
+        console.log("One");
+        break;
+
+    case 2:
+        console.log("Two");
+        break;
+
+    case 10:
+        console.log("Ten");
+        break;
+
+    default:
+        console.log("Not Found");
+        break;
+}
