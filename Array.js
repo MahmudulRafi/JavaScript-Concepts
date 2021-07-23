a8c970e8d88997d521e980c069403eac89841f48
MahmudulRafi <rafi.mahmudul.cse@gmail.com>
2021-07-23 11:49:18 +0600

    Array in JavaSrcipt

---
+++ Array.js
@@ -0,0 +1,22 @@
+// Array, IndexOf, Set by Index, Push, Pop, Length
+
+var numberList = [14, 11, 15, 20, 10];
+
+numberList[1] = 12;
+numberList.push(23);     // Push an element
+numberList.pop();        // Pop an element from array
+numberList.unshift(8);   // Add an element in begaining of the array
+numberList.shift();      // Remove an element from begaining of the array
+
+console.log(numberList);
+console.log(numberList.length);
+
+var position = numberList.indexOf(10);
+console.log(position);
+
+var nameList = ['Fuad', 'Muhaimin', 'Muntasir'];
+
+nameList.unshift('Rafi', 'Rafat');
+nameList.shift();
+
+console.log(nameList);
