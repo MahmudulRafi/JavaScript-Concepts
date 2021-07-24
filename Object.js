cbffb9d34ad17518b37c02d076f549f1358288a2
MahmudulRafi <rafi.mahmudul.cse@gmail.com>
2021-07-24 11:38:56 +0600

    Obejct in JavaScript

---
+++ Object.js
@@ -0,0 +1,18 @@
+// Object
+
+var employee101 = { employeeId: 101, name: "Mahmudul Hasan Rafi", phone: 178328334, department: "Software" }
+var employee102 = { employeeId: 102, name: "Mahadi Hasan Rafat", phone: 198328334, department: "HR" }
+
+employee101.phone = 1839549584;
+
+employee101.designation = "Engineer";
+employee102.designation = "Human Resource Manager";
+
+console.log(employee101);
+console.log(employee102);
+
+console.log(employee101.employeeId);
+console.log(employee101.name);
+console.log(employee101.phone);
+console.log(employee101.department);
+console.log(employee101.designation);
