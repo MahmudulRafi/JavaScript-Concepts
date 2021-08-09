6ad9f96af6ae8b3a4958c0933fd88d46254a6bb6
MahmudulRafi <rafi.mahmudul.cse@gmail.com>
2021-08-09 16:14:14 +0600

    Spread Operator

--- Spread-Operator.js
+++ Spread-Operator.js
@@ -2,9 +2,9 @@
 
 var array = [1, 2, 3, 4, 'Rafi'];
 
-var array2 = [...array, 'Rafat'];
+var array2 = [...array, 'Rafat'];   // Can implement previous data as reference in new array.
 
-var array3 = [...array];
+var array3 = [...array];            // It can stop editing the array from the core.
 
 array3.push(10);
 
